c8e6bd134540e01282dec649b2f71f47fcd95562
Bryan Chua <bryanchua0724@gmail.com>
2021-04-30 20:49:49 +0800

    Validating Fields

--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
 
-const sendBtn = document.getElementById('sendBtn');
-
-
-
+const sendBtn = document.getElementById('sendBtn'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
 
 
 eventListeners();
 
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', appInit);
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
-
-
-
-
 
 
 function appInit() {
     sendBtn.disabled = true;
 }
 
+function validateField() {
+    let errors;
 
+    validateLength(this);
+
+    if(this.type === 'email'){
+        validateEmail(this);
+    }
+}
+
+function validateLength(field) {
+    if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+
+function validateEmail(field) {
+    let emailText = field.value;
+
+    if(emailText.indexOf('@') !== -1 ) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
 
 
 
