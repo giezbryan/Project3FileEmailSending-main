8aff87fa4891ca2dd50306f5045c87f4b00a7e14
Bryan Chua <bryanchua0724@gmail.com>
2021-04-30 20:27:12 +0800

    Kicking off the project

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+const sendBtn = document.getElementById('sendBtn');
+
+
+
+
+
+eventListeners();
+
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', appInit);
+}
+
+
+
+
+
+
+function appInit() {
+    sendBtn.disabled = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
